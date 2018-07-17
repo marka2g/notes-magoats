170ce50498bb62a36136f304cb7a956fdfe009d4
marka2g <mark.sadegi@gmail.com>
2018-07-17 14:19:08 -0700

    gify api get and modal with notes

--- frontend/notes-magoats/src/components/GifItem.js
+++ frontend/notes-magoats/src/components/GifItem.js
@@ -1,5 +1,11 @@
 import React from 'react';
+
+// Currently, our GifItem is only expecting a prop object called image. Because image.onGifSelect() doesn't make as much sense, we're going to do a bit of ES2015 shorthand to assign any objects underneath image to their own vars.
+// const GifItem = (image) => {} - gives us image.gif and image.onGifSelect, we can write:
+// instead - const GifItem = ({gif, onGifSelect}) => {}
+// so we can access our props as gif and onGifSelect directly!
 const GifItem = ({gif, onGifSelect}) => {
+  // Next, we need our App component to know when a gif is clicked so that it can set selectedGif and modalIsOpen on state accordingly. Once again, we're going to have to pass a handler down into a child component as a prop, but this time, we'll have to do it twice, first to GifList and then to GifItem.
   return (
     <div className="gif-item" onClick={() => onGifSelect(gif)}>
       <img src={gif.images.downsized.url} />
--- frontend/notes-magoats/src/components/SearchBar.js
+++ frontend/notes-magoats/src/components/SearchBar.js
@@ -6,17 +6,25 @@ class SearchBar extends Component {
     this.state = {term: ''}
   }
 
+  // This is one of the most important things to remember when you're working with React. When you're initializing state in the constructor, you can set it directly with this.state = {}. However, if you want to signal to React that the state has changed so that it knows to re-render, you need to call this.setState() instead. This means you should never call this.state = {} outside of a class constructor.
   onInputChange(term) {
     this.setState({term});
+    // We need a way to pass data from a child component (SearchBar) to its parent (App), and in React, we can do that through props -- data or callbacks passed from a parent component.
     this.props.onTermChange(term);
   }
 
+  // fat-arrow functions - Here, we're telling React that every time it notices we've changed our input, it should fire an onChange event and pass the value — our search term — to our onInputChange() class method. Let's take a closer look at that now: {evt => this.onInputChange(evt.target.value)} similar to this:
+  // function(event) {
+  //   this.onInputChange(event.target.value);
+  // }
   render(){
     return(
       <div className="search">
-        <input onChange={evt => this.onInputChange(evt.target.value)} />
+        <input placeholder="Enter text to search for gifs!" onChange={evt => this.onInputChange(evt.target.value)} />
       </div>
     );
   }
 }
+// export makes our SearchBar available to import by other pieces of our application, as we are about to do in our App class.
+// The default means that this module is only exporting one value — in this case, the class SearchBar.
 export default SearchBar;
