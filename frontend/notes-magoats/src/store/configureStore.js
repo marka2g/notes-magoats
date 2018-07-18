89ff2a54f00ada2bdd8c3efc6830f21569cdf304
marka2g <mark.sadegi@gmail.com>
2018-07-18 12:41:11 -0700

    finish up action -> promise implementation

--- frontend/notes-magoats/src/store/configureStore.js
+++ frontend/notes-magoats/src/store/configureStore.js
@@ -1,11 +1,16 @@
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    window.devToolsExtension ? window.devToolsExtension() : undefined
+    // We need to use `compose()` if we're using multiple function transformations to enhance a store—in this case, Redux's applyMiddleware (to which we're passing the imported ReduxPromise) along with our code to enable Redux DevTools. With just those few extra lines of code, any promises we return from our actions should be fully resolved when they hit our reducers.
+    compose(
+      applyMiddleware(ReduxPromise),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
   );
 
   if (module.hot) {
