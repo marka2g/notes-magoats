cb68d762ea0b5c8b97caddc06bf6c6f5c6306499
marka2g <mark.sadegi@gmail.com>
2018-07-18 17:38:23 -0700

    half of auth, minus connecting the forms to redux connect

--- frontend/notes-magoats/src/reducers/index.js
+++ frontend/notes-magoats/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import gifsReducer from './gifs';
 import modalReducer from './modal';
+import authReducer from './auth';
 // add react-router-redux's routerReducer to our main reducers file so that it can intercept navigation actions and keep track of our current location in the store:
 import { routerReducer } from 'react-router-redux';
 // formReducer needs to access store too
-import { formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   form: formReducer,
   gifs: gifsReducer,
   modal: modalReducer,
