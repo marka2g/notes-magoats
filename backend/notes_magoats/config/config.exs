73f9f6d734a5c57cd65303e438b14278d37df6a4
marka2g <mark.sadegi@gmail.com>
2018-07-21 02:03:47 -0700

    add guardian

--- backend/notes_magoats/config/config.exs
+++ backend/notes_magoats/config/config.exs
@@ -14,14 +14,18 @@ config :notes_magoats, NotesMagoatsWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "8UDPA5CPq45u5uz6afzsHpqKoUeAQhrXMSGAhcLjwgLidD6qLbzRjtsW2W66DIgS",
   render_errors: [view: NotesMagoatsWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: NotesMagoats.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: NotesMagoats.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
+# Guardian config
+config :notes_magoats, NotesMagoats.Guardian,
+  issuer: "notes_magoats",
+  secret_key: "uiNGHRdb3aRmjU+cyh1ij8fevHO6IM9uAFtTMGMB+fS0Vp1OnGBKMmHWoQ2N9mQ/"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
