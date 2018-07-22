1517aec51be292da1732fe805e8c7e6e69b42b77
marka2g <mark.sadegi@gmail.com>
2018-07-21 18:23:44 -0700

    finish up generated gaurdian

--- backend/notes_magoats/lib/notes_magoats_web/controllers/user_controller.ex
+++ backend/notes_magoats/lib/notes_magoats_web/controllers/user_controller.ex
@@ -4,7 +4,7 @@ defmodule NotesMagoatsWeb.UserController do
   alias NotesMagoats.Accounts
   alias NotesMagoats.Accounts.User
 
-  action_fallback NotesMagoatsWeb.FallbackController
+  action_fallback(NotesMagoatsWeb.FallbackController)
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -12,11 +12,14 @@ defmodule NotesMagoatsWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", user_path(conn, :show, user))
-      |> render("show.json", user: user)
+    # with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+    with {:ok, %User{} = user} <- Accounts.create_user(user_params),
+         # conn
+         # |> put_status(:created)
+         # |> put_resp_header("location", user_path(conn, :show, user))
+         # |> render("show.json", user: user)
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
+      conn |> render("jwt.json", token)
     end
   end
 
@@ -35,6 +38,7 @@ defmodule NotesMagoatsWeb.UserController do
 
   def delete(conn, %{"id" => id}) do
     user = Accounts.get_user!(id)
+
     with {:ok, %User{}} <- Accounts.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
